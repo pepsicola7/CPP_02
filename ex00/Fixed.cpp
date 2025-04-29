af37a1fb387e191d8ae657e70aadbb426df34fc9
Prprkitty <168284039+pepsicola7@users.noreply.github.com>
2025-04-29 19:45:36 +0200

    add tout

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,8 +6,52 @@
 /*   By: peli <peli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 17:22:57 by peli              #+#    #+#             */
-/*   Updated: 2025/04/28 17:23:09 by peli             ###   ########.fr       */
+/*   Updated: 2025/04/29 19:45:19 by peli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+static const int	bits = 8;
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	fixed_point = 0;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& num)
+{
+	fixed_point = int(num.fixed_point);
+}
+
+Fixed&	Fixed::operator = (const Fixed& num)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &num)
+	{
+		this->fixed_point = num.fixed_point;
+	}
+	return (*this);
+}
+
+int	Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+
+}
+
+void	Fixed::setRawBits( int const raw )
+{
+
+}
+
+// ◦ A member function int getRawBits( void ) const;
+// that returns the raw value of the fixed-point value.
+// ◦ A member function void setRawBits( int const raw );
+// that sets the raw value of the fixed-point number.
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,8 +6,36 @@
 /*   By: peli <peli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/28 17:22:41 by peli              #+#    #+#             */
-/*   Updated: 2025/04/28 17:22:54 by peli             ###   ########.fr       */
+/*   Updated: 2025/04/29 19:45:19 by peli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return;
+}
+
+// Default constructor called
+// Copy constructor called
+// Copy assignment operator called // <-- This line may be missing depending on your implementation
+// getRawBits member function called
+// Default constructor called
+// Copy assignment operator called
+// getRawBits member function called
+// getRawBits member function called
+// 0
+// getRawBits member function called
+// 0
+// getRawBits member function called
+// 0
+// Destructor called
+// Destructor called
+// Destructor called
