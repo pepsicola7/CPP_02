b5787e882519d6e22ac12d7162ccedcf9718aae0
Peiqi Li <peli@c2r11p1.42nice.fr>
2025-05-15 11:18:42 +0200

    version to push

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: peli <peli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 16:10:35 by peli              #+#    #+#             */
-/*   Updated: 2025/05/14 18:13:51 by peli             ###   ########.fr       */
+/*   Updated: 2025/05/15 11:11:06 by peli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int main( void ) {
     {
-        +Fixed a;
+        Fixed a;
         Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
         std::cout << a << std::endl;
         std::cout << ++a << std::endl;
